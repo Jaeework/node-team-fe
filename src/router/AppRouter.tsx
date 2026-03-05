5f504cb7bd9552a0e711f1b2db205a2b703d87d5
Jaeework <jaeework@gmail.com>
2026-03-05 19:04:45 +0900

    chore: add AppLayout component

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import LandingPage from "../pages/LandingPage/LandingPage";
+import AppLayout from "../components/layout/AppLayout";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage /> } />
-
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<LandingPage />} />
+      </Route>
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
