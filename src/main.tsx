c22cda3ace0f8c1223d6dea90fabd1d29b629472
SoYoungLEE-me <lsy000626@gmail.com>
2026-03-07 01:52:15 +0900

    chore:ui-initial-setup(fonts, lucide)

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import "@fontsource/inter/variable.css";
+import "pretendard/dist/web/variable/pretendardvariable.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
