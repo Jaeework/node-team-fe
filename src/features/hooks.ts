1a332e5e9c692ae6e5fc528ed7920506226421e0
Jaeework <jaeework@gmail.com>
2026-03-08 15:37:17 +0900

    fix: #14 useAppSelector 제네릭 타입 추론 적용

--- src/features/hooks.ts
+++ src/features/hooks.ts
@@ -2,5 +2,5 @@ import { useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "./store";
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector = (selector: (state: RootState) => unknown) =>
+export const useAppSelector = <T>(selector: (state: RootState) => T): T =>
   useSelector(selector);
